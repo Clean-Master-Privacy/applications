84a02b7f177709cb6a42587314e3163d0b072686
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-11-03 21:04:01 +0300

    Update and rename swipe_favorite.json to swipe_favorite.cpp
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ assets/swipe_favorite.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <boost/optional.hpp>
+#include <stdexcept>
+#include <regex>
+
+namespace codebeautify {
+    using Welcome1 = std::map<std::string, std::vector<std::string>>;
+}
