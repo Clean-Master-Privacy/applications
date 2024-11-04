e08962f135e0b9817f1c1488e0e9332fe49cc592
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-11-04 20:59:07 +0300

    Delete assets/swipe_favorite.cpp
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

--- assets/swipe_favorite.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/optional.hpp>
-#include <stdexcept>
-#include <regex>
-
-namespace codebeautify {
-    using Welcome1 = std::map<std::string, std::vector<std::string>>;
-}
