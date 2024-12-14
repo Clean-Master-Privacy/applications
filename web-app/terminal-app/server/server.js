86d914e8c0e086468ce058170ff8a7a7c25b09d6
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2024-12-14 18:02:39 +0300

    Update server.js
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

--- web-app/terminal-app/server/server.js
+++ web-app/terminal-app/server/server.js
@@ -3,40 +3,37 @@ const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-// WebSocket server'ı başlat
-const wss = new WebSocket.Server({ port: 8080 });
+const port = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: port });
 
 wss.on('connection', ws => {
     ws.on('message', message => {
-        // Terminali temizleme komutu
         if (message === 'clear') {
-            ws.send("\x1b[H\x1b[2J");  // ANSI escape kodlarıyla terminali temizle
+            ws.send("\x1b[H\x1b[2J");
         } 
-        // ls komutu: Dizin içeriğini listeleme
         else if (message.startsWith('ls ')) {
-            const dirPath = message.slice(3);  // 'ls ' komutunu çıkar
-            const resolvedPath = path.resolve(dirPath);  // Mutlak yolu al
+            const dirPath = message.slice(3);
+            const resolvedPath = path.resolve(dirPath);
 
             fs.readdir(resolvedPath, (err, files) => {
                 if (err) {
-                    ws.send(`Error: ${err.message}`);  // Hata mesajını gönder
+                    ws.send(`Error: ${err.message}`);
                 } else {
                     const filePaths = files.map(file => path.join(resolvedPath, file)).join('\n');
-                    ws.send(filePaths);  // Dosya yollarını frontend'e gönder
+                    ws.send(filePaths);
                 }
             });
         } 
-        // Diğer komutlar: Normal komutları çalıştır
         else {
             exec(message, (error, stdout, stderr) => {
                 if (error) {
                     ws.send(`Error: ${stderr}`);
                 } else {
-                    ws.send(stdout);  // Çıktıyı frontend'e gönder
+                    ws.send(stdout);
                 }
             });
         }
     });
 });
 
-console.log('WebSocket server running on ws://localhost:8080');
+console.log(`WebSocket server running on ws://localhost:${port}`);
