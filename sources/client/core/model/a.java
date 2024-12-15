b277bafbfaf2b134fd8764e7dff960de70cf0c53
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-15 21:22:25 +0300

    Create a.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ sources/client/core/model/a.java
@@ -0,0 +1,16 @@
+    package client.core.model;
+
+public final class a<T> extends h {
+    private T jn = null;
+    private e<T> jo = null;
+
+    public a(T t, e<T> eVar) {
+        this.jn = t;
+        this.jo = eVar;
+    }
+
+    public final c bC() {
+        return this.jo.m(this.jn);
+    }
+}
+    
