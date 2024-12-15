98b5d7e57fda7988b38e0f82ef078dcc2c28c53f
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-15 17:26:54 +0300

    Create Database.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ sources/SQLite /Database.java
@@ -0,0 +1,24 @@
+package SQLite;
+
+import java.io.File;
+import java.io.PrintStream;
+
+public class Database {
+    public static native String version();
+
+    static {
+        String property;
+        try {
+            property = System.getProperty("SQLite.library.path");
+            if (property == null || property.length() == 0) {
+                System.loadLibrary("sqlite_jni");
+                return;
+            }
+            System.load(property + File.separator + ((String) System.class.getMethod("mapLibraryName", new Class[]{String.class}).invoke((Object) null, new Object[]{"sqlite_jni"})));
+        } catch (Throwable th) {
+            PrintStream printStream = System.err;
+            printStream.println("Unable to load sqlite_jni: " + th);
+        }
+    }
+}
+  
