990da973807d30ff3733154ca0f1b23a937efc83
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-15 21:28:23 +0300

    Create c.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ sources/f/c.java
@@ -0,0 +1,15 @@
+    package f;
+
+public final class c {
+    String contentType = "application/octet-stream";
+    String jPP;
+    String jPQ;
+    String jPR;
+
+    public c(String str, String str2, String str3) {
+        this.jPR = str2;
+        this.jPP = str;
+        this.jPQ = str3;
+    }
+}
+    
