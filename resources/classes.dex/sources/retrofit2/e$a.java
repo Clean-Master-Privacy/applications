5f49c1b011c9e39387053a680054601cca12fc28
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-16 20:07:04 +0300

    Create e$a.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/retrofit2/e$a.java
@@ -0,0 +1,20 @@
+    package retrofit2;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.annotation.Nullable;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+
+public abstract class e$a {
+    @Nullable
+    public e<ResponseBody, ?> a(Type type, Annotation[] annotationArr) {
+        return null;
+    }
+
+    @Nullable
+    public e<?, RequestBody> b(Type type) {
+        return null;
+    }
+}
+    
