2dfd3a6b3dda53f148c659fd24841ead876f2de5
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-16 20:04:22 +0300

    Create b.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/retrofit2/b.java
@@ -0,0 +1,14 @@
+    package retrofit2;
+
+import java.io.IOException;
+
+public interface b<T> extends Cloneable {
+    void a(d<T> dVar);
+
+    l<T> bYD() throws IOException;
+
+    b<T> bYE();
+
+    boolean isCanceled();
+}
+    
