4d2c4dd53aeb3741967c299ce234e021aec71450
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-16 21:34:02 +0300

    Create a.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/kotlin/a.java
@@ -0,0 +1,13 @@
+    package kotlin;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@a
+@Retention(RetentionPolicy.RUNTIME)
+public @interface a {
+}
+    
