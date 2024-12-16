dbc399ff284e47d4d9977d239d7733dfc5efcb41
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-16 22:04:03 +0300

    Create p.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/a/b/p.java
@@ -0,0 +1,35 @@
+    package com.airbnb.lottie.a.b;
+
+import com.airbnb.lottie.e.a;
+import com.airbnb.lottie.e.b;
+import com.airbnb.lottie.e.c;
+import java.util.Collections;
+
+public final class p<K, A> extends a<K, A> {
+    /* access modifiers changed from: package-private */
+    public final float cL() {
+        return 1.0f;
+    }
+
+    public p(c<A> cVar) {
+        super(Collections.emptyList());
+        new b();
+        a(cVar);
+    }
+
+    public final void cH() {
+        if (this.nz != null) {
+            super.cH();
+        }
+    }
+
+    public final A getValue() {
+        return this.nz.value;
+    }
+
+    /* access modifiers changed from: package-private */
+    public final A a(a<K> aVar, float f) {
+        return this.nz.value;
+    }
+}
+    
