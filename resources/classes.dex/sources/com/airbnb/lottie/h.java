f54f6df2a0dca90abc11da7dc70f27908c4c43eb
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-16 21:42:47 +0300

    Create h.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/h.java
@@ -0,0 +1,33 @@
+    package com.airbnb.lottie;
+
+import android.graphics.ColorFilter;
+import android.graphics.PointF;
+import com.airbnb.lottie.e.d;
+
+public interface h {
+    public static final Integer lO = 1;
+    public static final Integer lP = 2;
+    public static final Integer lQ = 3;
+    public static final Integer lR = 4;
+    public static final PointF lS = new PointF();
+    public static final PointF lT = new PointF();
+    public static final PointF lU = new PointF();
+    public static final PointF lV = new PointF();
+    public static final d lW = new d();
+    public static final Float lX = Float.valueOf(1.0f);
+    public static final Float lY = Float.valueOf(2.0f);
+    public static final Float lZ = Float.valueOf(3.0f);
+    public static final Float ma = Float.valueOf(4.0f);
+    public static final Float mb = Float.valueOf(5.0f);
+    public static final Float mc = Float.valueOf(6.0f);
+    public static final Float md = Float.valueOf(7.0f);
+    public static final Float me = Float.valueOf(8.0f);
+    public static final Float mf = Float.valueOf(9.0f);
+    public static final Float mg = Float.valueOf(10.0f);
+    public static final Float mh = Float.valueOf(11.0f);
+    public static final Float mi = Float.valueOf(12.0f);
+    public static final Float mj = Float.valueOf(12.0f);
+    public static final Float mk = Float.valueOf(13.0f);
+    public static final ColorFilter ml = new ColorFilter();
+}
+    
