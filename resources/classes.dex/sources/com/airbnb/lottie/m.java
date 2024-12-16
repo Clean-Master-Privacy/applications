e2a3a787c5716fb0b433ca2353d10e391b9536ff
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-16 21:45:54 +0300

    Create m.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/m.java
@@ -0,0 +1,12 @@
+    package com.airbnb.lottie;
+
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.support.annotation.ColorInt;
+
+public final class m extends PorterDuffColorFilter {
+    public m(@ColorInt int i) {
+        super(i, PorterDuff.Mode.SRC_ATOP);
+    }
+}
+    
