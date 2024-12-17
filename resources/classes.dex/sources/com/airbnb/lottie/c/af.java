481a91e4b80fc9de9546624b4cc2bb6679ca6236
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 20:54:04 +0300

    Create af.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/c/af.java
@@ -0,0 +1,9 @@
+    package com.airbnb.lottie.c;
+
+import android.util.JsonReader;
+import java.io.IOException;
+
+interface af<V> {
+    V a(JsonReader jsonReader, float f) throws IOException;
+}
+    
