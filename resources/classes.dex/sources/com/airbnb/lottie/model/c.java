cd4124ad899faa754912b4d640b2afcac64e29cf
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 21:26:21 +0300

    Create c.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/model/c.java
@@ -0,0 +1,17 @@
+    package com.airbnb.lottie.model;
+
+import android.support.annotation.RestrictTo;
+
+@RestrictTo({RestrictTo.Scope.LIBRARY})
+public final class c {
+    public final String name;
+    public final String oo;
+    public final String oq;
+
+    public c(String str, String str2, String str3) {
+        this.oo = str;
+        this.name = str2;
+        this.oq = str3;
+    }
+}
+    
