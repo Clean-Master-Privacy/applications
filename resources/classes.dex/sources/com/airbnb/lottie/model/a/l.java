2ec1a160e082cd1e1bc0c433cade7cf609782706
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 21:34:32 +0300

    Create l.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/model/a/l.java
@@ -0,0 +1,44 @@
+    package com.airbnb.lottie.model.a;
+
+import android.graphics.PointF;
+import android.support.annotation.Nullable;
+import com.airbnb.lottie.a.b.o;
+import com.airbnb.lottie.e;
+import com.airbnb.lottie.model.content.b;
+import com.airbnb.lottie.model.layer.a;
+
+public final class l implements b {
+    public final e oF;
+    public final m<PointF, PointF> oG;
+    public final g oH;
+    public final b oI;
+    public final d oJ;
+    @Nullable
+    public final b oK;
+    @Nullable
+    public final b oL;
+
+    @Nullable
+    public final com.airbnb.lottie.a.a.b a(e eVar, a aVar) {
+        return null;
+    }
+
+    public l() {
+        this(new e(), new e(), new g(), new b(), new d(), new b(), new b());
+    }
+
+    public l(e eVar, m<PointF, PointF> mVar, g gVar, b bVar, d dVar, @Nullable b bVar2, @Nullable b bVar3) {
+        this.oF = eVar;
+        this.oG = mVar;
+        this.oH = gVar;
+        this.oI = bVar;
+        this.oJ = dVar;
+        this.oK = bVar2;
+        this.oL = bVar3;
+    }
+
+    public final o cQ() {
+        return new o(this);
+    }
+}
+    
