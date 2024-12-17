ed235264db73ba783c47f6b36c7fabb667b98d03
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 21:32:01 +0300

    Create g.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/model/a/g.java
@@ -0,0 +1,29 @@
+    package com.airbnb.lottie.model.a;
+
+import com.airbnb.lottie.a.b.k;
+import com.airbnb.lottie.e.a;
+import com.airbnb.lottie.e.d;
+import java.util.List;
+
+public final class g extends n<d, d> {
+    public final /* bridge */ /* synthetic */ String toString() {
+        return super.toString();
+    }
+
+    g() {
+        this(new d(1.0f, 1.0f));
+    }
+
+    public g(d dVar) {
+        super(dVar);
+    }
+
+    public g(List<a<d>> list) {
+        super(list);
+    }
+
+    public final com.airbnb.lottie.a.b.a<d, d> cO() {
+        return new k(this.ny);
+    }
+}
+    
