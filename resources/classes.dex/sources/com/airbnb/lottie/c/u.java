4e0a6bc69951600aeff792665ef6af437e3841f7
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 21:13:08 +0300

    Create u.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/c/u.java
@@ -0,0 +1,17 @@
+    package com.airbnb.lottie.c;
+
+import android.graphics.PointF;
+import android.util.JsonReader;
+import java.io.IOException;
+
+public final class u implements af<PointF> {
+    public static final u qM = new u();
+
+    private u() {
+    }
+
+    public final /* synthetic */ Object a(JsonReader jsonReader, float f) throws IOException {
+        return n.c(jsonReader, f);
+    }
+}
+    
