01b3005304e9a655f7701b90a8aeed52765420d3
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 21:00:05 +0300

    Create i.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/c/i.java
@@ -0,0 +1,16 @@
+    package com.airbnb.lottie.c;
+
+import android.util.JsonReader;
+import java.io.IOException;
+
+public final class i implements af<Float> {
+    public static final i qH = new i();
+
+    private i() {
+    }
+
+    public final /* synthetic */ Object a(JsonReader jsonReader, float f) throws IOException {
+        return Float.valueOf(n.b(jsonReader) * f);
+    }
+}
+    
