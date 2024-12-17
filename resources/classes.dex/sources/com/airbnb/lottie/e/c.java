5483697de2cf72a3f4f6457f344795f31013dca3
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 21:24:11 +0300

    Create c.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ resources/classes.dex/sources/com/airbnb/lottie/e/c.java
@@ -0,0 +1,18 @@
+    package com.airbnb.lottie.e;
+
+import android.support.annotation.Nullable;
+
+public final class c<T> {
+    @Nullable
+    public T value = null;
+
+    public c() {
+        new b();
+    }
+
+    public c(@Nullable T t) {
+        new b();
+        this.value = t;
+    }
+}
+    
