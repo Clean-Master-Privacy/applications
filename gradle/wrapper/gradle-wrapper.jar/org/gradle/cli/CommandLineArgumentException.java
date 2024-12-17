c05fbb575257c6860ac963accc9385923468a4dd
gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>
2024-12-17 22:15:45 +0300

    Create CommandLineArgumentException.java
    Signed-off-by: gamestime102tv <180425951+gamestime102-tv@users.noreply.github.com>

---
+++ gradle/wrapper/gradle-wrapper.jar/org/gradle/cli/CommandLineArgumentException.java
@@ -0,0 +1,8 @@
+    package org.gradle.cli;
+
+public final class CommandLineArgumentException extends RuntimeException {
+   public CommandLineArgumentException(String var1) {
+      super(var1);
+   }
+}
+    
