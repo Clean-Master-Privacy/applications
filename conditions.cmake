a7c0b6d4b4cba3447a4ad67569d0933ab9a4f0da
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2024-12-27 20:00:11 +0300

    Update and rename conditions.make to conditions.cmake
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

--- conditions.cmake
+++ conditions.cmake
@@ -3,5 +3,3 @@ gamestime102
 Licensed under the GNU Affero General Public License v3.0 ("License");
 
 You may not use this file except in accordance with the License.
-
-You may obtain a copy of the License at:
