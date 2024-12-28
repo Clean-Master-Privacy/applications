d3db93bf33197887162277c55d9c14cb53950f7a
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2024-12-28 21:02:45 +0300

    Update nc_yt_player.js
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

--- android-app/assets/ytplayer/nc_yt_player.js
+++ android-app/assets/ytplayer/nc_yt_player.js
@@ -1,4 +1,4 @@
-var tag = document.createElement('script');
+    var tag = document.createElement('script');
 
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -63,3 +63,4 @@ var tag = document.createElement('script');
             player.playVideo();
         }
       }
+    
