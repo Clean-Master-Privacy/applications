21b7f4d4f307ef6f01d9b0f13285a42087a82afd
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-09 17:07:24 +0300

    Create Loan.cs
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ LibraryAutomation/Models/Loan.cs
@@ -0,0 +1,14 @@
+namespace LibraryAutomation.Models
+{
+    public class Loan
+    {
+        public int LoanId { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public User User { get; set; }
+        public Book Book { get; set; }
+    }
+}
