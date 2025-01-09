b8058ce876fe01d5abcc4ecb4dbea022daac9479
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-09 16:59:34 +0300

    Create BookController.cs
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ LibraryAutomation/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LibraryAutomation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public BookController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var books = await _context.Books.ToListAsync();
+            return Ok(books);
+        }
+    }
+}
