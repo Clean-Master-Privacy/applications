3919a48efcfe6f0129b720061ca489323efc412c
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-09 17:16:27 +0300

    Delete LibraryAutomation directory
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

--- LibraryAutomation/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace LibraryAutomation.Models
-{
-    public class User
-    {
-        public int UserId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
--- LibraryAutomation/read.md
+++
@@ -1,3 +0,0 @@
-Veritabanı migrasyonunu uygulamak için:
-dotnet ef migrations add InitialCreate
-dotnet ef database update
