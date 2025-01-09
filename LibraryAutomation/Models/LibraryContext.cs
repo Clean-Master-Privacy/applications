7d96bf5190978cc35b03b9c84596055e6b1266a9
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-09 17:08:18 +0300

    Create LibraryContext.cs
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ LibraryAutomation/Models/LibraryContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAutomation.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+    }
+}
