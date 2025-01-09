ae748620897ac0fe17623b4de1354dde7dab3809
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-09 17:00:29 +0300

    Create Book.cs
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ LibraryAutomation/Models/Book.cs
@@ -0,0 +1,11 @@
+namespace LibraryAutomation
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool AvailabilityStatus { get; set; }
+    }
+}
