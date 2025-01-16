d7a80b7ec31fa8cfb3289ca2fec7476bc57b1b68
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-16 22:35:13 +0300

    Update scripts.js
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -5,5 +5,25 @@ window.addEventListener('scroll', function() {
         if (element.getBoundingClientRect().top <= window.innerHeight) {
             element.classList.add('fade-in');
         }
+        
     });
 });
+
+// Hasaralma animasyonunun çalıştığı öğeyi seç
+const fadeInSections = document.querySelectorAll('.scroll-anim');
+
+// IntersectionObserver'ı kullanarak sayfada görünürlük durumunu kontrol et
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible'); // Sayfada görünür olduğunda 'visible' sınıfını ekle
+            observer.unobserve(entry.target); // Gözlemi sonlandır
+        }
+    });
+}, { threshold: 0.5 }); // Öğenin %50'si görünür olduğunda animasyonu başlat
+
+// Tüm öğeleri gözlemle
+fadeInSections.forEach(section => {
+    observer.observe(section);
+});
+
