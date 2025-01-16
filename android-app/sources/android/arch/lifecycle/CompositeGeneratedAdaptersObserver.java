39c30068c2a034595b94205dd2fcc9b8a775025f
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-16 15:29:32 +0300

    Create  CompositeGeneratedAdaptersObserver.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/arch/lifecycle/CompositeGeneratedAdaptersObserver.java
@@ -0,0 +1,12 @@
+package android.arch.lifecycle;
+
+import android.arch.lifecycle.Lifecycle;
+
+public class CompositeGeneratedAdaptersObserver implements GenericLifecycleObserver {
+    CompositeGeneratedAdaptersObserver() {
+    }
+
+    public final void a(d dVar, Lifecycle.Event event) {
+        new h();
+    }
+}
