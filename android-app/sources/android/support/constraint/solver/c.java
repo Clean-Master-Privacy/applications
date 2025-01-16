f4b596820e5630c20f0e51d515946915b797cd03
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-16 16:04:49 +0300

    Create c.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/support/constraint/solver/c.java
@@ -0,0 +1,9 @@
+package android.support.constraint.solver;
+
+import android.support.constraint.solver.f;
+
+public final class c {
+    f.a<b> cH = new f.b();
+    f.a<SolverVariable> cI = new f.b();
+    SolverVariable[] cJ = new SolverVariable[32];
+}
