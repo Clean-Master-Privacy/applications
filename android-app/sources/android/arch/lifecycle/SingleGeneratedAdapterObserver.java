e9259978fd159e7aa51ab99356b1ddf6d45cab21
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-16 15:38:32 +0300

    Create SingleGeneratedAdapterObserver.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/arch/lifecycle/SingleGeneratedAdapterObserver.java
@@ -0,0 +1,11 @@
+package android.arch.lifecycle;
+
+import android.arch.lifecycle.Lifecycle;
+
+public class SingleGeneratedAdapterObserver implements GenericLifecycleObserver {
+    public final void a(d dVar, Lifecycle.Event event) {
+    }
+
+    SingleGeneratedAdapterObserver() {
+    }
+}
