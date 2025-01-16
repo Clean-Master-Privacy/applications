d4e904cbd4c213365215a523ff4bd441bc32dd30
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-16 15:36:41 +0300

    Create n.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/arch/lifecycle/n.java
@@ -0,0 +1,17 @@
+package android.arch.lifecycle;
+
+/* compiled from: ViewModelProvider */
+public final class n {
+    public final a aU;
+    public final o mViewModelStore;
+
+    /* compiled from: ViewModelProvider */
+    public interface a {
+        <T extends m> T T();
+    }
+
+    public n(o oVar, a aVar) {
+        this.aU = aVar;
+        this.mViewModelStore = oVar;
+    }
+}
