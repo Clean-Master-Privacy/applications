5e83e2cfe1cf24995429cb23a99f30fc948aeaff
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-16 20:30:57 +0300

    Create c.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/support/design/c/c.java
@@ -0,0 +1,13 @@
+package android.support.design.c;
+
+import android.view.View;
+
+public final class c {
+    public boolean expanded = false;
+    public final View hL;
+    public int hM = 0;
+
+    public c(b bVar) {
+        this.hL = (View) bVar;
+    }
+}
