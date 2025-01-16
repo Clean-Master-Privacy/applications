1a2b632c78fd1569b91213e6b36e1f3f3cffe09f
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-16 15:33:19 +0300

    Create i.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/arch/lifecycle/i.java
@@ -0,0 +1,12 @@
+package android.arch.lifecycle;
+
+/* compiled from: MutableLiveData */
+public class i<T> extends LiveData<T> {
+    public final void d(T t) {
+        super.d(t);
+    }
+
+    public void setValue(T t) {
+        super.setValue(t);
+    }
+}
