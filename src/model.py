c7036d77b858d65d21eac595978213456282050f
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-17 17:43:26 +0300

    Update model.py
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

--- src/model.py
+++ src/model.py
@@ -1,14 +1,14 @@
 # model.py
 import tensorflow as tf
-from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras import layers, models
+from tensorflow.keras.applications import MobileNetV2
 
 def create_model(input_shape=(224, 224, 3), num_classes=10):
     # MobileNetV2 modelini yüklüyoruz, önceden eğitilmiş ağırlıkları kullanıyoruz
     base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=input_shape)
     
     # Modelin üst kısmını serbest bırakıyoruz (eğitilmesi için)
-    base_model.trainable = False
+    base_model.trainable = False  # Başlangıçta yalnızca üst katmanları eğitmeye başlayacağız
 
     # Yeni sınıflar için üst katmanlar ekliyoruz
     model = models.Sequential([
@@ -18,6 +18,7 @@ def create_model(input_shape=(224, 224, 3), num_classes=10):
         layers.Dense(num_classes, activation='softmax')
     ])
 
+    # Modeli derliyoruz
     model.compile(optimizer=tf.keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
