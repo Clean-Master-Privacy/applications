abf6759f5aa8c9ba99ea8e9141d1714527cf48b6
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-17 20:59:01 +0300

    Create IPackageDataObserver.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/content/pm/IPackageDataObserver.java
@@ -0,0 +1,89 @@
+package android.content.pm;
+
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+public abstract interface IPackageDataObserver
+  extends IInterface
+{
+  public abstract void onRemoveCompleted(String paramString, boolean paramBoolean)
+    throws RemoteException;
+  
+  public static abstract class Stub
+    extends Binder
+    implements IPackageDataObserver
+  {
+    private static final String DESCRIPTOR = "android.content.pm.IPackageDataObserver";
+    static final int TRANSACTION_onRemoveCompleted = 1;
+    
+    public Stub()
+    {
+      attachInterface(this, "android.content.pm.IPackageDataObserver");
+    }
+    
+    public static IPackageDataObserver asInterface(IBinder paramIBinder)
+    {
+      if (paramIBinder == null) {
+        return null;
+      }
+      IInterface localIInterface = paramIBinder.queryLocalInterface("android.content.pm.IPackageDataObserver");
+      if ((localIInterface != null) && ((localIInterface instanceof IPackageDataObserver))) {
+        return (IPackageDataObserver)localIInterface;
+      }
+      return new a(paramIBinder);
+    }
+    
+    public IBinder asBinder()
+    {
+      return this;
+    }
+    
+    public boolean onTransact(int paramInt1, Parcel paramParcel1, Parcel paramParcel2, int paramInt2)
+      throws RemoteException
+    {
+      if (paramInt1 != 1)
+      {
+        if (paramInt1 != 1598968902) {
+          return super.onTransact(paramInt1, paramParcel1, paramParcel2, paramInt2);
+        }
+        paramParcel2.writeString("android.content.pm.IPackageDataObserver");
+        return true;
+      }
+      paramParcel1.enforceInterface("android.content.pm.IPackageDataObserver");
+      paramParcel2 = paramParcel1.readString();
+      boolean bool;
+      if (paramParcel1.readInt() != 0) {
+        bool = true;
+      } else {
+        bool = false;
+      }
+      onRemoveCompleted(paramParcel2, bool);
+      return true;
+    }
+    
+    private static final class a
+      implements IPackageDataObserver
+    {
+      private IBinder mRemote;
+      
+      a(IBinder paramIBinder)
+      {
+        mRemote = paramIBinder;
+      }
+      
+      public final IBinder asBinder()
+      {
+        return mRemote;
+      }
+      
+      public final void onRemoveCompleted(String paramString, boolean paramBoolean)
+        throws RemoteException
+      {
+        throw new Runtime("d2j fail translate: java.lang.RuntimeException: can not merge I and Z\n\tat com.googlecode.dex2jar.ir.TypeClass.merge(TypeClass.java:100)\n\tat com.googlecode.dex2jar.ir.ts.TypeTransformer$TypeRef.updateTypeClass(TypeTransformer.java:174)\n\tat com.googlecode.dex2jar.ir.ts.TypeTransformer$TypeAnalyze.copyTypes(TypeTransformer.java:311)\n\tat com.googlecode.dex2jar.ir.ts.TypeTransformer$TypeAnalyze.fixTypes(TypeTransformer.java:226)\n\tat com.googlecode.dex2jar.ir.ts.TypeTransformer$TypeAnalyze.analyze(TypeTransformer.java:207)\n\tat com.googlecode.dex2jar.ir.ts.TypeTransformer.transform(TypeTransformer.java:44)\n\tat com.googlecode.d2j.dex.Dex2jar$2.optimize(Dex2jar.java:162)\n\tat com.googlecode.d2j.dex.Dex2Asm.convertCode(Dex2Asm.java:414)\n\tat com.googlecode.d2j.dex.ExDex2Asm.convertCode(ExDex2Asm.java:42)\n\tat com.googlecode.d2j.dex.Dex2jar$2.convertCode(Dex2jar.java:128)\n\tat com.googlecode.d2j.dex.Dex2Asm.convertMethod(Dex2Asm.java:509)\n\tat com.googlecode.d2j.dex.Dex2Asm.convertClass(Dex2Asm.java:406)\n\tat com.googlecode.d2j.dex.Dex2Asm.convertDex(Dex2Asm.java:422)\n\tat com.googlecode.d2j.dex.Dex2jar.doTranslate(Dex2jar.java:172)\n\tat com.googlecode.d2j.dex.Dex2jar.to(Dex2jar.java:272)\n\tat com.googlecode.dex2jar.tools.Dex2jarCmd.doCommandLine(Dex2jarCmd.java:108)\n\tat com.googlecode.dex2jar.tools.BaseCmd.doMain(BaseCmd.java:288)\n\tat com.googlecode.dex2jar.tools.Dex2jarCmd.main(Dex2jarCmd.java:32)\n");
+      }
+    }
+  }
+}
