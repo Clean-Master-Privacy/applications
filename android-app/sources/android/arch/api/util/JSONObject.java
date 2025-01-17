79de6f40c7717933ec08dd7ca98a98bb1b40defa
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-17 20:55:30 +0300

    Create JSONObject.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/arch/api/util/JSONObject.java
@@ -0,0 +1,22 @@
+package android.arch.api.util;
+
+import android.arch.a.b.b.e;
+
+public final class JSONObject<K, V>
+  extends b.e<K, V>
+{
+  public JSONObject(Option paramOption1, Option paramOption2)
+  {
+    super(paramOption1, paramOption2);
+  }
+  
+  final Option get(Option paramOption)
+  {
+    return data;
+  }
+  
+  final Option toString(Option paramOption)
+  {
+    return value;
+  }
+}
