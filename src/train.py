d4b4373ae7e6b8c27aa97856a68f94b418b8edaa
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-17 17:44:18 +0300

    Update train.py
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

--- src/train.py
+++ src/train.py
@@ -5,10 +5,10 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.datasets import cifar10
 from model import create_model
 
-# Veri setini yükleyelim (CIFAR-10 örneği)
+# CIFAR-10 veri setini yükleyelim
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# Veriyi uygun şekilde yeniden şekillendiriyoruz
+# Veriyi uygun şekilde yeniden şekillendiriyoruz (CIFAR-10, 32x32, biz ise 224x224 istiyoruz)
 X_train = tf.image.resize(X_train, (224, 224))
 X_test = tf.image.resize(X_test, (224, 224))
 
@@ -25,10 +25,10 @@ model = create_model(input_shape=(224, 224, 3), num_classes=10)
 # Modeli eğitiyoruz (ilk aşama: transfer öğrenme)
 model.fit(X_train, y_train, epochs=5, batch_size=32, validation_data=(X_test, y_test))
 
-# Transfer öğrenme sonrası modelin üst katmanlarını serbest bırakıyoruz (sürekli öğrenme)
-model.trainable = True
-for layer in model.layers[:100]:  # Önceden eğitimli katmanları donmuş tutuyoruz
-    layer.trainable = False
+# Modelin üst katmanlarını serbest bırakıyoruz (sürekli öğrenme)
+model.trainable = True  # Tüm katmanları serbest bırakıyoruz
+for layer in model.layers[0].layers:  # MobileNetV2 katmanlarını sabit tutuyoruz
+    layer.trainable = False  # Sadece üst katmanları eğitmeye devam edeceğiz
 
 # Yeni veri ile modelin eğitimini sürdürüyoruz (sürekli öğrenme)
 model.fit(X_train, y_train, epochs=5, batch_size=32, validation_data=(X_test, y_test))
