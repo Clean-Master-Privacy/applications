24f19be35cec4c49b8495bd65493d3ca0ddcdbef
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-17 20:52:57 +0300

    Create BufferedTreeNodeStream.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/arch/api/util/BufferedTreeNodeStream.java
@@ -0,0 +1,22 @@
+package android.arch.api.util;
+
+import android.arch.a.b.b.e;
+
+final class BufferedTreeNodeStream<K, V>
+  extends b.e<K, V>
+{
+  BufferedTreeNodeStream(Option paramOption1, Option paramOption2)
+  {
+    super(paramOption1, paramOption2);
+  }
+  
+  final Option get(Option paramOption)
+  {
+    return value;
+  }
+  
+  final Option toString(Option paramOption)
+  {
+    return data;
+  }
+}
