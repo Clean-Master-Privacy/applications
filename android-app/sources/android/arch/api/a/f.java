ce4d67586dfa0159b5e949f7f489dbe52c452d76
gamestime102 <180412020+gamestime102@users.noreply.github.com>
2025-01-17 20:50:37 +0300

    Create f.java
    Signed-off-by: gamestime102 <180412020+gamestime102@users.noreply.github.com>

---
+++ android-app/sources/android/arch/api/a/f.java
@@ -0,0 +1,12 @@
+package android.arch.api.a;
+
+public abstract class f
+{
+  public f() {}
+  
+  public abstract void a(Runnable paramRunnable);
+  
+  public abstract void close(Runnable paramRunnable);
+  
+  public abstract boolean close();
+}
